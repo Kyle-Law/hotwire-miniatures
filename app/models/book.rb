e1570a351249647eebad625c6ff706cb3c4976e3
Kyle-Law <kylelaw1121@gmail.com>
2021-05-05 22:39:34 +0800

    Book-Add: Setup basic book layout

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  after_create_commit { broadcast_append_to "books" }
+
+  after_update_commit { broadcast_replace_to "books" }
+
+  after_destroy_commit { broadcast_remove_to "books" }
 end
